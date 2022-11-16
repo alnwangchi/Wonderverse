9233af8c27c39bb468a18a3eba1828e5afbe0b0c
molly huang <molly.huang@shoalter.com>
2022-11-17 01:52:23 +0800

    輪播

--- content/js/common.js
+++ content/js/common.js
@@ -5,4 +5,32 @@ $(document).ready(function () {
   });
 
   $('.go_top').click(() => window.scrollTo({ top: 0, behavior: 'smooth' }));
+
+  // 輪播區塊
+  let oMain = document.querySelector('.carousel_main');
+  let oUl = oMain.getElementsByTagName('ul')[0];
+  let oLi = oUl.getElementsByTagName('li');
+
+  let space = 10;
+  oUl.innerHTML = oUl.innerHTML + oUl.innerHTML;
+  oUl.style.width = oLi[0].offsetWidth * oLi.length + 'px';
+
+  function move() {
+    if (oUl.offsetLeft < -oUl.offsetWidth / 2) {
+      oUl.style.left = 0;
+    }
+    if (oUl.offsetLeft > 0) {
+      oUl.style.left = -oUl.offsetWidth / 2 + 'px';
+    }
+    oUl.style.left = oUl.offsetLeft - space + 'px';
+  }
+  let timer = setInterval(move, 30);
+
+  oMain.onmouseover = function () {
+    space = 30;
+  };
+  //  開始輪播
+  oMain.onmouseout = function () {
+    space = 10;
+  };
 });
