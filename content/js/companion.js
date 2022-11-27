054feaaea1f625a49f33d6b0d322932999b00220
chunwen <hsbsbbdn@gmail.com>
2022-11-27 16:18:23 +0800

    feat: add fluid tab

--- content/js/companion.js
+++ content/js/companion.js
@@ -1,17 +1,16 @@
 const container = document.querySelector(".tab");
-const buttons = document.querySelectorAll(".tab-btn");
+const labels = document.querySelectorAll(".tab-btn");
 const contents = document.querySelectorAll(".content");
 
 container.addEventListener("click", (e) => {
   const id = e.target.dataset.id;
   const element = document.getElementById(id);
   if (id) {
-    buttons.forEach((item) => {
+    labels.forEach((item) => {
       item.classList.remove("btn-active");
     });
     e.target.classList.add("btn-active");
     e.target.querySelector("img").classList.add("active");
-
     contents.forEach((item) => {
       item.classList.remove("active");
     });
